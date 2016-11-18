e6a6314d0c952a334ff1c742b4c772607cf6abe5
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2016-11-18 14:21:46 -0500

    add UTCdatetime utils
    Change-Id: I24026a7e5bdce78ddf2d183438ebd8323cb90ce6

---
+++ core_oaipmh_common_app/utils/UTCdatetime.py
@@ -0,0 +1,34 @@
+import iso8601
+
+
+def datetime_to_utc_datetime_iso8601(datetime, day_granularity=False):
+    """ Convert a datetime into its iso8601 UTC date.
+
+        Parameters:
+            datetime: Datetime to convert.
+            day_granularity: Apply the day granularity
+
+        Returns:
+            Converted date (string).
+
+    """
+    # ignore microseconds
+    datetime = datetime.replace(microsecond=0)
+    result = datetime.isoformat() + 'Z'
+    if day_granularity:
+        result = result[:-10]
+    return result
+
+
+def utc_datetime_iso8601_to_datetime(utc_datetime):
+    """ Convert an iso8601 UTC date into datetime.
+
+        Parameters:
+            utc_datetime: iso8601 UTC date to convert.
+
+        Returns:
+            Converted date (Datetime).
+
+    """
+    return iso8601.parse_date(utc_datetime).replace(tzinfo=None)
+
---
+++ core_oaipmh_common_app/utils/tests/tests_iso8601_operation.py
@@ -0,0 +1,40 @@
+"""
+    Iso8601 operation test class
+"""
+
+from unittest import TestCase
+from core_oaipmh_common_app.utils import UTCdatetime
+import datetime
+
+
+class TestDatetimeToUTCDatetimeIso8601(TestCase):
+    def test_valid_conversion(self):
+        # Arrange
+        date_to_convert = datetime.datetime(2016, 11, 18, 8, 30)
+
+        # Act
+        result = UTCdatetime.datetime_to_utc_datetime_iso8601(date_to_convert)
+
+        # Assert
+        self.assertEquals(result, "2016-11-18T08:30:00Z")
+
+
+class TestUTCDatetimeIso8601ToDatetime(TestCase):
+    def test_valid_conversion(self):
+        # Arrange
+        date_to_convert = "2016-11-18T08:30:00Z"
+
+        # Act
+        result = UTCdatetime.utc_datetime_iso8601_to_datetime(date_to_convert)
+
+        # Assert
+        self.assertEquals(result, datetime.datetime(2016, 11, 18, 8, 30))
+
+    def test_invalid_conversion(self):
+        # Arrange
+        date_to_convert = "20166-11-18T08:30:00Z"
+
+        # Act # Assert
+        with self.assertRaises(Exception):
+            UTCdatetime.utc_datetime_iso8601_to_datetime(date_to_convert)
+
