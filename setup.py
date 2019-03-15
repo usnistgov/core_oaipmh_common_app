336cbdfeae203d96a2f63095413fffe3e7460966
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-03-14 22:18:45 -0400

    fix: core 1.0.1 sets kombu version to avoid broken celery dependency
    Change-Id: Ic9f3a2afbd955ba740cadd25ccec09e1b7b9bda1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_oaipmh_common_app",
-    version="1.0.0",
+    version="1.0.1",
     description="Base OAI-PMH functions for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
