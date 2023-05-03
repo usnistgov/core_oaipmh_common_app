8e161497b6a1d8022ef2ac902f17987e0bd6e80d
Guillaume Sousa <guillaume.sousa@nist.gov>
2023-05-03 02:06:33 +0000

    chore: upgrade 2.4.0
    Change-Id: I08fdca32d6b708507b2b8adcac725ff6d0964949

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_oaipmh_common_app",
-    version="2.3.0",
+    version="2.4.0",
     description="Base OAI-PMH functions for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
